105796369f2e9a37b35d7900c0135cd4c3ba4294
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-09 20:05:58 -0300

    Enviando Parâmetros na Rota

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/contato/{nome}/{categoria}/{assunto}/{mensagem}', function(string $nome, string $categoria, string $assunto, string $mensagem) {
+    echo "Estamos aqui: $nome - $categoria - $assunto - $mensagem";
+});

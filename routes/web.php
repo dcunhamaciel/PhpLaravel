1ac6787d02a1152f4a993aa3693c2ae2a53b6f3a
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-12 08:44:45 -0300

    Criando Rotas

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+
+Route::get('/login', function() { return 'Login'; });
+Route::get('/clientes', function() { return 'Clientes'; });
+Route::get('/fornecedores', function() { return 'Fornecedores'; });
+Route::get('/produtos', function() { return 'Produtos'; });

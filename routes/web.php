75556e03214c7a15191c5e28f295e617ce3cbe87
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-12 08:41:53 -0300

    Menu de Navegação

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,3 @@ Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])
 Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
 
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
-
-Route::get('/contato/{nome}/{categoria_id}', function(string $nome, int $categoria_id = 1) {
-    echo "Estamos aqui: $nome - $categoria_id";
-})->where('nome', '[A-Za-z]+')->where('categoria_id', '[0-9]+');

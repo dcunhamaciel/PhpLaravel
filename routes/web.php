290526b96408f238e29d2ec4dc88d4857290a907
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-12 09:00:49 -0300

    Nomeando Rotas

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,16 @@ use PHPUnit\TextUI\XmlConfiguration\Group;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal']);
+Route::get('/', [\App\Http\Controllers\PrincipalController::class, 'principal'])->name('site.index');
 
-Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos']);
+Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
 
-Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato']);
+Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 
-Route::get('/login', function() { return 'Login'; });
+Route::get('/login', function() { return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
-    Route::get('/clientes', function() { return 'Clientes'; });
-    Route::get('/fornecedores', function() { return 'Fornecedores'; });
-    Route::get('/produtos', function() { return 'Produtos'; });
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });

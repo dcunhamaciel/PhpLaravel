a79060079984c651f3290c2e2166ee8f5ec6c560
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-14 18:27:19 -0300

    Blade @unless

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -7,8 +7,10 @@ use Illuminate\Http\Request;
 class FornecedorController extends Controller
 {
     public function index() {
-        $fornecedores = ['Fornecedor 1'];
-        
+        $fornecedores = [
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N']
+        ];
+
         return view("app.fornecedor.index", compact('fornecedores'));
     }
 }
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,9 +1,21 @@
 <h3>Fornecedor</h3>
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>    
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else
-    <h3>Ainda não existem fornecedores cadastrados</h3>
+@php
+    /*
+    if(!<condicao>) {} //executa se o retorno for true
+    */
+@endphp
+
+{{-- @unless executa se o retorno for false --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br/>
+Status: {{ $fornecedores[0]['status'] }}
+<br/>
+@if(!($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo.
 @endif
+<br/>
+@unless(($fornecedores[0]['status'] == 'S'))
+    Fornecedor inativo.
+@endunless

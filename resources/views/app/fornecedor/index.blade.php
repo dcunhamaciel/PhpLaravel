6ec8816b416d36dbce3048d2d554174e27efc436
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-14 13:14:48 -0300

    Comentários em Blade

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,10 @@
+<h3>Fornecedor</h3>
+
+{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
+
+@php
+    // Para comentários de uma linha
+    /*
+        Para comentários de múltiplas linhas
+    */
+@endphp
--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/login', function() { return 'Login'; })->name('site.login');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
-    Route::get('/fornecedores', function() { return 'Fornecedores'; })->name('app.fornecedores');
+    Route::get('/fornecedores', [\App\Http\Controllers\FornecedorController::class, 'index']);
     Route::get('/produtos', function() { return 'Produtos'; })->name('app.produtos');
 });
 

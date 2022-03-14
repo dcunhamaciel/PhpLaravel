bf6254e1e6818bbc46a37f4049013d94984a8d29
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-14 18:19:44 -0300

    Blade @if/@else

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,10 +1,9 @@
 <h3>Fornecedor</h3>
 
-{{-- Fica o comentário que será descartado pelo interpretador do blade --}}
-
-@php
-    // Para comentários de uma linha
-    /*
-        Para comentários de múltiplas linhas
-    */
-@endphp
+@if(count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>    
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif

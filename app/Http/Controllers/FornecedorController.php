11897726e6b10a90ad121ca69c43fdd5de539e42
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-14 20:27:11 -0300

    Blade @empty

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -8,7 +8,7 @@ class FornecedorController extends Controller
 {
     public function index() {
         $fornecedores = [
-            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '00.000.000/0000-00'],
+            0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => ''],
             1 => ['nome' => 'Fornecedor 1', 'status' => 'N']
         ];
 
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,18 +2,29 @@
 
 @php
     /*
-    if(isset($variavel)) {} //retorna true se a variável estiver definida
+    if(empty($variavel)) {} //retorna true se a variável estiver vazia
+    - '' string vazia
+    - 0 inteiro zero
+    - 0.0 ponto flutuante zero
+    - '0' string contendo zero
+    - null
+    - false
+    - array() vazio
+    - $var variável somente declarada
     */
 @endphp
 
 {{-- @unless executa se o retorno for false --}}
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
+    Fornecedor: {{ $fornecedores[0]['nome'] }}
     <br/>
-    Status: {{ $fornecedores[1]['status'] }}
+    Status: {{ $fornecedores[0]['status'] }}
     <br/>
-    @isset($fornecedores[1]['cnpj'])
-        CNPJ: {{ $fornecedores[1]['cnpj'] }}
+    @isset($fornecedores[0]['cnpj'])
+        CNPJ: {{ $fornecedores[0]['cnpj'] }}
+        @empty($fornecedores[0]['cnpj'])
+            Vazio
+        @endempty
     @endisset    
 @endisset    

1a569a1c3dd302eff15aa1972ae9a6ab71733943
Diego Maciel <dcunhamaciel@gmail.com>
2022-03-15 19:56:10 -0300

    Blade @while

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -17,12 +17,18 @@
 {{-- @unless executa se o retorno for false --}}
 
 @isset($fornecedores)
-    @for($i = 0; isset($fornecedores[$i]); $i++)
+    @php
+        $i = 0;
+    @endphp
+    @while(isset($fornecedores[$i]))
         Fornecedor: {{ $fornecedores[$i]['nome'] }}
         <br/>
         Status: {{ $fornecedores[$i]['status'] }}
         <br/>
         Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[1]['telefone'] ?? '' }}
         <hr>
-    @endfor
+        @php
+            $i++;
+        @endphp        
+    @endwhile
 @endisset

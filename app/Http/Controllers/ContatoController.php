07c7e55e28f0cd9128e4c86bbcb88205b4ca930d
Diego Maciel <dcunhamaciel@gmail.com>
2022-04-11 19:41:20 -0300

    Gravando os Dados do Formulário no BD

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+        $contato->save();
+        */
+
+        $contato = new SiteContato();
+        $contato->fill($request->all());
+        $contato->save();
+
         return view('site.contato');
     }
 }

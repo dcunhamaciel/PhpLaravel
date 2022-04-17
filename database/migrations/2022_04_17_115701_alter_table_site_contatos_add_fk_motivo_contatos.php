5bb560177d97c0f93c0fdf7843f55064667b398f
dcunhamaciel <dcunhamaciel@gmail.com>
2022-04-17 09:07:34 -0300

    Refactoring

---
+++ database/migrations/2022_04_17_115701_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->unsignedBigInteger('motivo_contatos_id');            
+        });
+
+        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
+
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->foreign('motivo_contatos_id')->references('id')->on('motivo_contatos');
+            $table->dropColumn('motivo_contato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->integer('motivo_contato');
+            $table->dropForeign('site_contatos_motivo_contatos_id_foreign');            
+        });
+
+        DB::statement('update site_contatos set motivo_contato = motivo_contatos_id');
+
+        Schema::create('site_contatos', function (Blueprint $table) {
+            $table->dropColumn('motivo_contatos_id');
+        });        
+    }
+};

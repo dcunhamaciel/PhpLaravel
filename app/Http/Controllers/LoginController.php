a68da012b5e34e74db00e45b75686cda5819d7cc
Diego Maciel <dcunhamaciel@gmail.com>
2022-04-22 17:40:41 -0300

    Formulário de Login

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index() 
+    {
+        return view('site.login', ['titulo' => 'login']);
+    }
+
+    public function autenticar() 
+    {
+        return 'Autenticando Usuário...';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,8 @@ Route::get('/sobre-nos', [\App\Http\Controllers\SobreNosController::class, 'sobr
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class, 'contato'])->name('site.contato');
 Route::post('/contato', [\App\Http\Controllers\ContatoController::class, 'salvar'])->name('site.contato');
 
-Route::get('/login', function() { return 'Login'; })->name('site.login');
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'autenticar'])->name('site.login');
 
 Route::middleware('autenticacao:padrao')->prefix('/app')->group(function() {
     Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');

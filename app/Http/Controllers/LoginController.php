d8c345a1d739125ba755d306d24d9c03566f42aa
Diego Maciel <dcunhamaciel@gmail.com>
2022-04-23 09:52:40 -0300

    Recbendo parâmetros de usuário e senha

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,8 +11,20 @@ class LoginController extends Controller
         return view('site.login', ['titulo' => 'login']);
     }
 
-    public function autenticar() 
+    public function autenticar(Request $request) 
     {
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required'
+        ];
+
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
+            'senha.required' => 'O campo senha é obrigatório'
+        ];
+
+        $request->validate($regras, $feedback);
+        
         return 'Autenticando Usuário...';
     }
 }

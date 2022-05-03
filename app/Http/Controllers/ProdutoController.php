fc5f092297b1ff7b55d58dfccdcb4ce547d40e94
Diego Maciel <dcunhamaciel@gmail.com>
2022-05-03 20:24:13 -0300

    Cadastro de Produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -40,7 +40,9 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Produto::create($request->all());
+
+        return redirect()->route('produto.index');
     }
 
     /**

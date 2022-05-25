656f44d849de872675ccde77e8c2d4461578371e
dcunhamaciel <dcunhamaciel@gmail.com>
2022-05-25 19:27:17 -0300

    Utilizando hasOne e belongsTo com nomes não padronizados

---
+++ app/Models/ItemDetalhe.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ItemDetalhe extends Model
+{
+    use HasFactory;
+
+    protected $table = 'produto_detalhes';
+    protected $fillable = ['produto_id', 'comprimento', 'altura', 'largura', 'unidade_id'];
+
+    public function produto() 
+    {
+        return $this->belongsTo('App\Models\Item', 'produto_id', 'id');
+    }    
+}

f0cabd90b6efed26c11d9f3cbe0021dc2d47dc51
dcunhamaciel <dcunhamaciel@gmail.com>
2022-06-16 08:17:58 -0300

    Inserindo Registros por Meio do Relacionamento

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -40,19 +40,26 @@ class PedidoProdutoController extends Controller
     public function store(Request $request, Pedido $pedido)
     {
         $regras = [
-            'produto_id' => 'exists:produtos,id'
+            'produto_id' => 'exists:produtos,id',
+            'quantidade' => 'required'
         ];
 
         $feedback = [
-            'produto_id.exists' => 'O produto informado não existe'
+            'produto_id.exists' => 'O produto informado não existe',
+            'required' => 'O campo :attribute deve possuir um valor válido'
         ];
 
         $request->validate($regras, $feedback);
 
-        $pedidoProduto = new PedidoProduto();
+        /*$pedidoProduto = new PedidoProduto();
         $pedidoProduto->pedido_id = $pedido->id;
         $pedidoProduto->produto_id = $request->get('produto_id');
-        $pedidoProduto->save();
+        $pedidoProduto->quantidade = $request->get('quantidade');
+        $pedidoProduto->save();*/
+
+        $pedido->produtos()->attach(
+            $request->get('produto_id'),
+            ['quantidade' => $request->get('quantidade')]);
 
         return redirect()->route('pedido-produto.create', ['pedido' => $pedido->id]);
     }

ea5a2788b37ada518b456d7d45c8d479db592705
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-12 15:04:27 -0300

    Manipulando os dados exportados linha por linha e formatando datas

--- 02.ControleTarefas/app/Exports/TarefasExport.php
+++ 02.ControleTarefas/app/Exports/TarefasExport.php
@@ -5,8 +5,9 @@ namespace App\Exports;
 use App\Models\Tarefa;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
-class TarefasExport implements FromCollection, WithHeadings
+class TarefasExport implements FromCollection, WithHeadings, WithMapping
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -16,15 +17,28 @@ class TarefasExport implements FromCollection, WithHeadings
         return auth()->user()->tarefas()->get();
     }
 
+    /**
+     * @return array
+     */    
     public function headings(): array
     {
         return [
             'ID Tarefa',
             'Tarefa',
-            'Data Limite Conclusão',
-            'Data Inclusão',
-            'Data Alteração',
-            'ID Usuário'
+            'Data Limite Conclusão'
+        ];
+    }
+
+    /**
+     * @param  mixed  $row
+     * @return array
+     */
+    public function map($row): array
+    {
+        return [
+            $row->id,
+            $row->tarefa,
+            date('d-m-Y', strtotime($row->data_limite_conclusao))
         ];
     }
 }

210fd4d5bd801a959161e934794a70dea174874c
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-17 15:00:09 -0300

    Removendo registros via DELETE

--- 04.LocadoraDeCarros/app/Http/Controllers/MarcaController.php
+++ 04.LocadoraDeCarros/app/Http/Controllers/MarcaController.php
@@ -65,6 +65,8 @@ class MarcaController extends Controller
      */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+
+        return ['msg' => 'A marca foi removida com sucesso!'];
     }
 }

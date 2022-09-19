405ef9a26ba9dc9286334691441b53b5673f7b8d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-19 18:51:58 -0300

    Implementando as regras de validação (Rules) no Model

--- 04.LocadoraDeCarros/app/Models/Marca.php
+++ 04.LocadoraDeCarros/app/Models/Marca.php
@@ -10,4 +10,21 @@ class Marca extends Model
     use HasFactory;
 
     protected $fillable = ['nome', 'imagem'];
+
+    public function rules(): array
+    {
+        return [
+            'nome' => 'required|min:3|unique:marcas',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback(): array
+    {
+        return [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.min' => 'O nome deve ter no mínimo 3 caracteres',
+            'nome.unique' => 'O nome da marca já existe'
+        ];
+    }
 }

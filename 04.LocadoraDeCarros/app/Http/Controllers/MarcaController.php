5480fddd3712613d3bb9fb6a89a510f7d000fa8d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-19 18:45:32 -0300

    Validações - Validando parâmetros e a importância do Accept

--- 04.LocadoraDeCarros/app/Http/Controllers/MarcaController.php
+++ 04.LocadoraDeCarros/app/Http/Controllers/MarcaController.php
@@ -39,6 +39,18 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
+        $regras = [
+            'nome' => 'required|unique:marcas',
+            'imagem' => 'required'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute é obrigatório',
+            'nome.unique' => 'O nome da marca já existe'
+        ];
+
+        $request->validate($regras, $feedback);
+
         $marca = $this->marca->create($request->all());
 
         return response()->json($marca, Response::HTTP_CREATED);

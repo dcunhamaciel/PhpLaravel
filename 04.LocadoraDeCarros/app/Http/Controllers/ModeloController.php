8325a00d0f6c1b6361c304090ea860d9b73709e7
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-20 20:46:25 -0300

    API WebService Rest para o Resource Modelo

--- 04.LocadoraDeCarros/app/Http/Controllers/ModeloController.php
+++ 04.LocadoraDeCarros/app/Http/Controllers/ModeloController.php
@@ -4,9 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Models\Modelo;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 
 class ModeloController extends Controller
 {
+    private Modelo $modelo;
+
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;        
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +23,13 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+        $modelos = $this->modelo::all();
+
+        $httpStatusCode = empty($modelos) 
+            ? Response::HTTP_NO_CONTENT 
+            : Response::HTTP_OK;
+        
+        return response()->json($modelos, $httpStatusCode);
     }
 
     /**
@@ -25,7 +40,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules(), $this->modelo->feedback());
+
+        $imagem = $request->file('imagem');
+        $imagemUrn = $imagem->store('imagens/modelos', 'public');
+
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagemUrn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($modelo, Response::HTTP_CREATED);
     }
 
     /**
@@ -34,9 +64,15 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show(int $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if (empty($modelo)) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($modelo, Response::HTTP_OK);
     }
 
     /**
@@ -46,9 +82,46 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, int $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if (empty($modelo)) {
+            return response()->json(['erro' => 'Não foi possível realizar a atualização. Recurso solicitado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+        
+        if ($request->method() === 'PATCH') {
+            $regrasPatch = [];
+
+            foreach ($modelo->rules() as $field => $rule) {
+                if (array_key_exists($field, $request->all())) {
+                    $regrasPatch[$field] = $rule;
+                }
+            }
+
+            $request->validate($regrasPatch, $modelo->feedback());
+        } else {
+            $request->validate($modelo->rules(), $modelo->feedback());
+        }
+
+        if ($request->file('image')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $imagem = $request->file('imagem');
+        $imagemUrn = $imagem->store('imagens/modelos', 'public');        
+        
+        $modelo->update([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagemUrn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+
+        return response()->json($modelo, Response::HTTP_OK);
     }
 
     /**
@@ -57,8 +130,18 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy(int $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if (empty($modelo)) {
+            return response()->json(['erro' => 'Não foi possível realizar a exclusão. Recurso solicitado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        Storage::disk('public')->delete($modelo->imagem);
+
+        $modelo->delete();
+
+        return response()->json(['msg' => 'O modelo foi removido com sucesso!'], Response::HTTP_ACCEPTED);
     }
 }
--- 04.LocadoraDeCarros/app/Models/Modelo.php
+++ 04.LocadoraDeCarros/app/Models/Modelo.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['marca_id', 'nome', 'imagem', 'numero_portas', 'lugares', 'air_bag', 'abs'];
+
+    public function rules(): array
+    {
+        return [
+            'marca_id' => 'exists:marca,id',
+            'nome' => 'required|min:3|unique:modelos,nome,' . ($this->id ?? 0),
+            'imagem' => 'required|file|mimes:png,jpg,jpeg',
+            'numero_portas' => 'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bag' => 'required|boolean',
+            'abs' => 'required|boolean'
+        ];
+    }
+
+    public function feedback(): array
+    {
+        return [];
+    }
 }

a68b272f366f98ce2ac992126d00fb964032928a
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-27 19:27:55 -0300

    API WebService Rest para o Resource Carro

--- 04.LocadoraDeCarros/app/Http/Controllers/CarroController.php
+++ 04.LocadoraDeCarros/app/Http/Controllers/CarroController.php
@@ -2,20 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Storage;
 use App\Models\Carro;
-use App\Http\Requests\StoreCarroRequest;
-use App\Http\Requests\UpdateCarroRequest;
+use App\Repositories\CarroRepository;
 
 class CarroController extends Controller
 {
+    private Carro $carro;
+
+    public function __construct(Carro $carro)
+    {
+        $this->carro = $carro;
+    }
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $carroRepository = new CarroRepository($this->carro);
+
+        $atributosModelo = '';
+
+        if ($request->has('atributos_modelo')) {
+            $atributosModelo = 'modelo:id,' . $request->atributos_modelo;                        
+        } else {
+            $atributosModelo = 'modelo';
+        }
+
+        $carroRepository->selectAtributosRegistrosRelacionados($atributosModelo);
+
+        if ($request->has('filtro')) {
+            $filtro = $request->filtro;
+            $carroRepository->addFiltro($filtro);
+        }
+
+        if ($request->has('atributos')) {
+            $atributos = $request->atributos;            
+            $carroRepository->selectAtributos($atributos);
+        }
+                       
+        $carros = $carroRepository->getResultado();
+
+        $httpStatusCode = empty($carros) 
+            ? Response::HTTP_NO_CONTENT 
+            : Response::HTTP_OK;
+        
+        return response()->json($carros, $httpStatusCode);
     }
 
     /**
@@ -24,9 +61,18 @@ class CarroController extends Controller
      * @param  \App\Http\Requests\StoreCarroRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreCarroRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate($this->carro->rules(), $this->carro->feedback());
+
+        $carro = $this->carro->create([
+            'modelo_id' => $request->modelo_id,
+            'placa' => $request->placa,
+            'disponivel' => $request->disponivel,
+            'km' => $request->km
+        ]);
+
+        return response()->json($carro, Response::HTTP_CREATED);
     }
 
     /**
@@ -35,9 +81,15 @@ class CarroController extends Controller
      * @param  \App\Models\Carro  $carro
      * @return \Illuminate\Http\Response
      */
-    public function show(Carro $carro)
+    public function show(int $id)
     {
-        //
+        $carro = $this->carro->with('modelo')->find($id);
+
+        if (empty($carro)) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        return response()->json($carro, Response::HTTP_OK);
     }
 
     /**
@@ -47,9 +99,32 @@ class CarroController extends Controller
      * @param  \App\Models\Carro  $carro
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateCarroRequest $request, Carro $carro)
+    public function update(Request $request, int $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if (empty($carro)) {
+            return response()->json(['erro' => 'Não foi possível realizar a atualização. Recurso solicitado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+        
+        if ($request->method() === 'PATCH') {
+            $regrasPatch = [];
+
+            foreach ($carro->rules() as $field => $rule) {
+                if (array_key_exists($field, $request->all())) {
+                    $regrasPatch[$field] = $rule;
+                }
+            }
+
+            $request->validate($regrasPatch, $carro->feedback());
+        } else {
+            $request->validate($carro->rules(), $carro->feedback());
+        }
+       
+        $carro->fill($request->all());
+        $carro->save();
+
+        return response()->json($carro, Response::HTTP_OK);
     }
 
     /**
@@ -58,8 +133,16 @@ class CarroController extends Controller
      * @param  \App\Models\Carro  $carro
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Carro $carro)
+    public function destroy(int $id)
     {
-        //
+        $carro = $this->carro->find($id);
+
+        if (empty($carro)) {
+            return response()->json(['erro' => 'Não foi possível realizar a exclusão. Recurso solicitado não existe.'], Response::HTTP_NOT_FOUND);
+        }
+
+        $carro->delete();
+
+        return response()->json(['msg' => 'O carro foi removido com sucesso!'], Response::HTTP_ACCEPTED);
     }
 }
--- 04.LocadoraDeCarros/app/Models/Carro.php
+++ 04.LocadoraDeCarros/app/Models/Carro.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Carro extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['modelo_id', 'placa', 'disponivel', 'km'];
+
+    public function modelo()
+    {
+        return $this->belongsTo(Modelo::class);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'modelo_id' => 'exists:modelos,id',
+            'placa' => 'required',
+            'disponivel' => 'required',
+            'km' => 'required'
+        ];
+    }
+
+    public function feedback(): array
+    {
+        return [];
+    }    
 }

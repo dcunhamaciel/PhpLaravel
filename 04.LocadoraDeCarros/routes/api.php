d5c9e677d087ee9ac9c9bfb62b5d90ae2e1c758e
dcunhamaciel <dcunhamaciel@gmail.com>
2022-09-28 21:12:27 -0300

    Criando as rotas de autenticação e autorização e o AuthController

--- 04.LocadoraDeCarros/routes/api.php
+++ 04.LocadoraDeCarros/routes/api.php
@@ -28,3 +28,8 @@ Route::apiResource('cliente', ClienteController::class);
 Route::apiResource('locacao', LocacaoController::class);
 Route::apiResource('marca', MarcaController::class);
 Route::apiResource('modelo', ModeloController::class);
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
+Route::post('me', 'App\Http\Controllers\AuthController@me');

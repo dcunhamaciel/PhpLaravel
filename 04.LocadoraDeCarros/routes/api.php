0bd2a904adefef50c5ed370297661c033639119c
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-01 09:48:41 -0300

    Configurando rotas protegidas por autorização

--- 04.LocadoraDeCarros/routes/api.php
+++ 04.LocadoraDeCarros/routes/api.php
@@ -23,13 +23,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('carro', CarroController::class);
-Route::apiResource('cliente', ClienteController::class);
-Route::apiResource('locacao', LocacaoController::class);
-Route::apiResource('marca', MarcaController::class);
-Route::apiResource('modelo', ModeloController::class);
-
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
 Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
 Route::post('me', 'App\Http\Controllers\AuthController@me');
+
+Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::apiResource('carro', CarroController::class);
+    Route::apiResource('cliente', ClienteController::class);
+    Route::apiResource('locacao', LocacaoController::class);
+    Route::apiResource('marca', MarcaController::class);
+    Route::apiResource('modelo', ModeloController::class);
+});

2fa7e2619d3d6f0275deed9da25559ce058d45fc
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-01 09:36:45 -0300

    Implementando o método de login (autenticação) e o JWT (autorização)

--- 04.LocadoraDeCarros/app/Http/Controllers/AuthController.php
+++ 04.LocadoraDeCarros/app/Http/Controllers/AuthController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AuthController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
-        return 'login';
+        $credenciais = $request->all(['email', 'password']);
+        
+        $token = auth('api')->attempt($credenciais);
+
+        if (!$token) {
+            return response()->json(['erro' => 'usuário ou senha inválidos'], Response::HTTP_FORBIDDEN);
+        }
+
+        return response()->json(['token' => $token], Response::HTTP_OK);
     }
 
     public function logout()

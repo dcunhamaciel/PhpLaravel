65e9b870bdd121a0929d992f42d40068dd530aaa
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-01 10:06:35 -0300

    Recuperando os dados do usuário autenticado com base no JWT

--- 04.LocadoraDeCarros/routes/api.php
+++ 04.LocadoraDeCarros/routes/api.php
@@ -26,9 +26,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('login', 'App\Http\Controllers\AuthController@login');
 Route::post('logout', 'App\Http\Controllers\AuthController@logout');
 Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-Route::post('me', 'App\Http\Controllers\AuthController@me');
 
 Route::prefix('v1')->middleware('jwt.auth')->group(function() {
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
     Route::apiResource('carro', CarroController::class);
     Route::apiResource('cliente', ClienteController::class);
     Route::apiResource('locacao', LocacaoController::class);

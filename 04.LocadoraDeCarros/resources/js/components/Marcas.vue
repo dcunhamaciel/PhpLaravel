1dd170b37ee2da0d52f006607fb935830af238ca
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-03 20:26:30 -0300

    Criando o card de busca de registros

--- 04.LocadoraDeCarros/resources/js/components/Marcas.vue
+++ 04.LocadoraDeCarros/resources/js/components/Marcas.vue
@@ -3,10 +3,25 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Marcas</div>
+                    <div class="card-header">Busca de Marcas</div>
 
                     <div class="card-body">
-                        <p>Listar Marcas</p>
+                        <div class="form-row">
+                            <div class="col mb-3">
+                                <label for="inputId" class="form-label">ID</label>
+                                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                                <div id="idHelp" class="form-text">Opcional. Informe o ID do Registro</div>
+                            </div>
+                            <div class="col mb-3">
+                                <label for="inputNome" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp" placeholder="Nome">
+                                <div id="nomeHelp" class="form-text">Opcional. Informe o Nome do Registro</div>
+                            </div>                        
+                        </div>
+                    </div>
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-sm float-end">Pesquisar</button>
                     </div>
                 </div>
             </div>

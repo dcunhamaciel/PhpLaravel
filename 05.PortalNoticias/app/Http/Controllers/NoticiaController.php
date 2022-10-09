93406d76abb3009ca1a563928a57e88e0312ba0a
dcunhamaciel <dcunhamaciel@gmail.com>
2022-10-09 15:58:44 -0300

    Armazenando consultas em cache

--- 05.PortalNoticias/app/Http/Controllers/NoticiaController.php
+++ 05.PortalNoticias/app/Http/Controllers/NoticiaController.php
@@ -20,12 +20,16 @@ class NoticiaController extends Controller
         //$site = Cache::get('site');
         //echo $site;        
         
-        if (Cache::has('dez_primeiras_noticias')) {
-            $noticias = Cache::get('dez_primeiras_noticias');
-        } else {
-            $noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
-            Cache::put('dez_primeiras_noticias', $noticias, 20);
-        }     
+        //if (Cache::has('dez_primeiras_noticias')) {
+            //$noticias = Cache::get('dez_primeiras_noticias');
+        //} else {
+            //$noticias = Noticia::orderByDesc('created_at')->limit(10)->get();
+            //Cache::put('dez_primeiras_noticias', $noticias, 20);
+        //}     
+
+        $noticias = Cache::remember('dez_primeiras_noticias', 20, function() {
+            return Noticia::orderByDesc('created_at')->limit(10)->get();
+        });
 
         return view('noticia', ['noticias' => $noticias]);
     }
